dbfbe596b32a04d4a77d1bd0097f53da4fd80f77
Andrew <andrewbeaton123@gmail.com>
2024-01-16 22:34:47 +0000

    Added prime factors and list summary

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     
     println!("Guess the number !");
 
-    println!("Please input the guess.");
+    println!("Please input your integer guess.");
 
     let mut guess: String = String::new();
     io::stdin() 
@@ -16,15 +16,63 @@ fn main() {
 
     println!("You double what you have  is  {}", sum(int_guess, int_guess));
     //sum(int_guess, int_guess))
-
-    let half_int_guess : i32 = int_guess.checked_div(2).unwrap_or(0);
+    let float_guess: f32 =  int_guess as f32 ;
     
-    println!("Divided by two that numver is {}",half_int_guess);
+    let half_flt_guess : f32 = float_guess/2 as f32;
+    
+    println!("Divided by two that numver is {}",half_flt_guess);
+
+    if int_guess <=20  {
+        let factorial_int_guess :i128 = factorial(int_guess as i128) ;
+
+        println!("The factorial of your guess is {}", factorial_int_guess);
+    }else {
+        println!("The value entered would likely overflow an i128 an so no factorial has been clalculated")
+    }
+    
+    println!("The triple of that number is {}", list_sum(vec![int_guess,int_guess,int_guess])) ;
+
+    print!("The prime factors of your value are {:?}", prime_factorization(int_guess)) ;
 }
+
+fn factorial(n_long: i128) -> i128{
+    // returns the factorial of the entered number 
+    let n_long: i128 = n_long as i128 ; 
+
+    if n_long == 0 || n_long ==1{
+        1 
+    }else {
+     n_long*factorial(n_long-1)
+    }
+}   
+
+fn list_sum (v_n : Vec<i32>) -> i32 {
+    
+    let mut sum: i32  = 0; 
+    for &num in &v_n {
+        sum += num ; }
+    sum 
+} 
 
 
 fn sum(n1: i32, n2 : i32) -> i32{
 
     // adds n1 and n2 and returns 
     n1 + n2
+}
+
+fn prime_factorization (mut n : i32) -> Vec<i32>{
+
+    let mut factors = Vec :: new() ;
+    let mut divisor = 2 ;
+
+    while n > 1{
+        while n % divisor ==0 {
+            factors.push(divisor);
+            n /=divisor ;
+        }
+        divisor +=1 ;
+    }
+    factors
+
 }
